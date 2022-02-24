6c932daa40914aad0879f474f3970c7e542118ac
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-02-25 00:20:14 +0600

    brekup with var

--- basic.js
+++ basic.js
@@ -1,1 +1,51 @@
-var deposit = 400; 
+// variable
+var deposit = 400;
+// condition 
+if (deposit > 500){
+
+}
+if(deposit < 200){
+
+}
+else if(deposit == 500){
+
+}
+else if(deposit != 500){
+
+}
+else if(deposit >= 500){
+
+}
+else if(deposit <= 500) {
+
+}
+else {
+
+}
+
+// array 
+const numbers = [45,6587,124,45,1,365];
+const numberCount = numbers.length;
+numbers.pop();
+numbers.push(111);
+numbers[2] = 555;
+// check wheather 222 included in the array 
+if(numbers.indexOf(222) != -1){
+
+}
+
+// loop
+// while, for
+
+for (const number of numbers){
+
+}
+
+// function
+function fullName (first, second){
+    const name = first +'' +second;
+}
+const person = fullName('Rahim', 'Ali');
+
+// object 
+const bottle = {color : 'yellow', contain: 'water', price: '50'}; 
